66ce89bba910e3f6a3e0b5326b6803a2fefcaafd
Adrian Hareńczyk <adrian.harenczyk@gmail.com>
2018-04-07 19:49:56 +0200

    Testing multiplication.

--- TheMoneyExample/src/test/java/PolyCurrencyTest.java
+++ TheMoneyExample/src/test/java/PolyCurrencyTest.java
@@ -1,2 +1,22 @@
+import org.junit.Test;
+import static junit.framework.TestCase.*;
+
 public class PolyCurrencyTest {
+
+    @Test
+    public void testMultiplication() {
+        // Given
+        Dollar five = new Dollar(5);
+
+        // When
+        five.times(2);
+
+        // Then
+        assertEquals(10, five.amount);
+
+        five.times(3);
+
+        assertEquals(15,five.amount);
+    }
+
 }
